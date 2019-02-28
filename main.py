83d4081bd3a68e274de9763745350207b6195077
kyle java <42562960+kylejava@users.noreply.github.com>
2019-02-28 09:28:06 -0800

    Found out how to add items into the users_city list

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 
 
 def main():
+        number_of_cities = 0
         users_city = []
         city = raw_input("Enter Name of city: ")
+        users_city.append(city)
+        new_city = raw_input("Would you like to enter another city? (Y/N)")
+        while(new_city == "y"):
+            city = raw_input("Enter name of city: ")
+            users_city.append(city)
+            new_city = raw_input("Would you like to enter another city? (Y/N)")
+        print(users_city)
 main()
