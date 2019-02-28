6267ef641b7269494d3762e35009bc2fff9becb0
kyle java <42562960+kylejava@users.noreply.github.com>
2019-02-28 09:31:42 -0800

    took out number of cities

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 
 def main():
-        number_of_cities = 0
+
         users_city = []
         city = raw_input("Enter Name of city: ")
         users_city.append(city)
@@ -11,5 +11,4 @@ def main():
             city = raw_input("Enter name of city: ")
             users_city.append(city)
             new_city = raw_input("Would you like to enter another city? (Y/N)")
-        print(users_city)
 main()
