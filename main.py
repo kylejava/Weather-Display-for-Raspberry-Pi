04a98d207745b0680947955380d18ff25724194e
kyle java <42562960+kylejava@users.noreply.github.com>
2019-02-28 09:39:09 -0800

    Tried to make add city a function

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 
 
+def add_city(city , users_city):
+        city = raw_input("Enter name of city: ")
+        users_city.append(city)
+        new_city = raw_input("Would you like to enter another city? (Y/N)")
+
 
 def main():
-
         users_city = []
         city = raw_input("Enter Name of city: ")
         users_city.append(city)
         new_city = raw_input("Would you like to enter another city? (Y/N)")
         while(new_city == "y"):
-            city = raw_input("Enter name of city: ")
-            users_city.append(city)
-            new_city = raw_input("Would you like to enter another city? (Y/N)")
+            print(add_city(users_city))
 main()
