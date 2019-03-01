0725757b5e54eb68e64f4dc8201c59e2d573b706
JVAVmusic <kyleedwardgjava@gmail.com>
2019-02-28 20:44:06 -0800

    was able to create an outside function

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-
-
+import datetime
+import time
+import pytz
 def add_city(city , users_city):
-        city = raw_input("Enter name of city: ")
+        city = input("Enter name of city: ")
         users_city.append(city)
-        new_city = raw_input("Would you like to enter another city? (Y/N)")
+        return(users_city)
 
 
 def main():
+        time = datetime.datetime.utcnow().strftime("%H:%M:%S")
+        LCD = True
         users_city = []
-        city = raw_input("Enter Name of city: ")
+        city = input("Enter Name of city: ")
         users_city.append(city)
-        new_city = raw_input("Would you like to enter another city? (Y/N)")
-        while(new_city == "y"):
-            print(add_city(users_city))
+        new_city = input("Would you like to enter another city? (Y/N)")
+        if(new_city != "y"):
+            print(users_city)
+        else:
+
+            while(new_city == "y"):
+                add_city(city, users_city)
+                new_city = input("Would you like to enter another city? (Y/N)")
+                print(users_city)
+        while(LCD == True):
+            print(users_city[0])
+            print(("Time in ") + (users_city[0]) + str(datetime.datetime.utcnow().strftime("%H:%M:%S")))
+
 main()
