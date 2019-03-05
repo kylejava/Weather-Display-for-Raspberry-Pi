f2db83f770abf45a7d60d52c3d1006c9c522f607
JVAVmusic <kyleedwardgjava@gmail.com>
2019-03-04 21:18:28 -0800

    Trying to get weather API to work

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@ import requests
 
 
 
-api_address='api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3371747f2cc96f96f53f3da617aa3f91'
+api_address='http://api.openweathermap.org/data/2.5/weather?q=&appid=3371747f2cc96f96f53f3da617aa3f91'
 
 city = input('City Name :')
 
@@ -10,6 +10,6 @@ url = api_address + city
 
 json_data = requests.get(url).json()
 
-format_add = json_data['base']
+format_add = json_data[0]
 
-print(format_add)
+print(json_data)
