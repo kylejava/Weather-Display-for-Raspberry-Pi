1a749990117bba48ff02feb149dd7643397b1b4f
JVAVmusic <kyleedwardgjava@gmail.com>
2019-03-04 21:22:21 -0800

    Did it! Thank you GitHub

--- weather.py
+++ weather.py
@@ -1,15 +1,63 @@
+#import requests
+
+
+
+#api_address='http://api.openweathermap.org/data/2.5/weather?q=&appid=3371747f2cc96f96f53f3da617aa3f91'
+
+#city = input('City Name :')
+
+#url = api_address + city
+
+#json_data = requests.get(url).json()
+
+#format_add = json_data[0]
+
+#print(json_data)
+
 import requests
 
+from pprint import pprint
 
 
-api_address='http://api.openweathermap.org/data/2.5/weather?q=&appid=3371747f2cc96f96f53f3da617aa3f91'
 
-city = input('City Name :')
+city = input('Enter your city : ')
 
-url = api_address + city
 
-json_data = requests.get(url).json()
 
-format_add = json_data[0]
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=3371747f2cc96f96f53f3da617aa3f91&units=metric'.format(city)
 
-print(json_data)
+
+
+res = requests.get(url)
+
+
+
+data = res.json()
+
+
+
+temp = data['main']['temp']
+
+wind_speed = data['wind']['speed']
+
+
+
+latitude = data['coord']['lat']
+
+longitude = data['coord']['lon']
+
+
+
+description = data['weather'][0]['description']
+
+
+
+print('Temperature : {} degree celcius'.format(temp))
+
+print('Wind Speed : {} m/s'.format(wind_speed))
+
+print('Latitude : {}'.format(latitude))
+
+print('Longitude : {}'.format(longitude))
+
+print('Description : {}'.format(description))
