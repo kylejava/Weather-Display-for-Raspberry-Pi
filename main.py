0adabe89ee1a00eb826a7c72b8557460515de073
JVAVmusic <kyleedwardgjava@gmail.com>
2019-03-08 19:03:43 -0800

    trying to see if i need to make weather a list

--- main.py
+++ main.py
@@ -10,10 +10,23 @@ import requests
 import sys
 from pprint import pprint
 
-def add_city(city , users_city):
-        city = input("Enter name of city: ")
-        users_city.append(city)
-        return(users_city)
+def add_city(weather_for_city , users_city):
+    city = input("Enter name of city: ")
+    users_city.append(city)
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=3371747f2cc96f96f53f3da617aa3f91&units=metric'.format(city)
+    res = requests.get(url)
+    data = res.json()
+    temp = data['main']['temp']
+    description = data['weather'][0]['description']
+    cel = 'Temperature : {} degree celcius'
+    new_far = (((temp) *(9/5)+32))
+    weather_for_city= []
+    weather_for_city.append(new_far)
+    return(users_city)
+    return (weather_for_city)
+
+
+
 
 
 def main():
@@ -23,9 +36,6 @@ def main():
     users_city.append(city)
     new_city = input("Would you like to enter another city? (Y/N)")
 
-
-
-
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=3371747f2cc96f96f53f3da617aa3f91&units=metric'.format(city)
     res = requests.get(url)
     data = res.json()
@@ -34,15 +44,19 @@ def main():
     cel = 'Temperature : {} degree celcius'
     far = (((temp) *(9/5)+32))
 
+    weather_for_city= []
+    weather_for_city.append(far)
     if(new_city != "y"):
-        while(LCD == True):
+        while(LCD == False):
             print("Weather in " + users_city[0] + ": "+ str(far))
             print("Time: " + datetime.datetime.now().strftime("%H:%M:%S"))
     else:
         while(new_city == "y"):
             add_city(city, users_city)
+
             new_city = input("Would you like to enter another city? (Y/N)")
-        while(LCD == True):
+
+        while(LCD == False):
             users_city_len = len(users_city)
             for i in range(0 , users_city_len):
                 i = 0
@@ -55,5 +69,6 @@ def main():
                         print(("Weather in ") + str(users_city[i]) + (": ")+ str(far))
                 else:
                     pass
-
+    print(users_city)
+    print(weather_for_city)
 main()
