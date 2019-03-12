2bad825bfef6018d7f20a01d6630db28056ef7c6
JVAVmusic <kyleedwardgjava@gmail.com>
2019-03-11 21:45:21 -0700

    Finally got to import multiple weather

--- main.py
+++ main.py
@@ -12,19 +12,28 @@ from pprint import pprint
 
 def add_city(weather_for_city , users_city):
     city = input("Enter name of city: ")
-    users_city.append(city)
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=3371747f2cc96f96f53f3da617aa3f91&units=metric'.format(city)
     res = requests.get(url)
     data = res.json()
     temp = data['main']['temp']
     description = data['weather'][0]['description']
     cel = 'Temperature : {} degree celcius'
-    new_far = (((temp) *(9/5)+32))
-    weather_for_city= []
-    weather_for_city.append(new_far)
-    return(users_city)
-    return (weather_for_city)
+    far = (((temp) *(9/5)+32))
 
+    users_city.append(city)
+    weather_for_city.append(str(far))
+    return(users_city)
+
+def add_weather(city , weather_for_city):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=3371747f2cc96f96f53f3da617aa3f91&units=metric'.format(city)
+    res = requests.get(url)
+    data = res.json()
+    temp = data['main']['temp']
+    description = data['weather'][0]['description']
+    cel = 'Temperature : {} degree celcius'
+    far = (((temp) *(9/5)+32))
+    weather_for_city.append(far)
+    return(weather_for_city)
 
 
 
@@ -52,7 +61,7 @@ def main():
             print("Time: " + datetime.datetime.now().strftime("%H:%M:%S"))
     else:
         while(new_city == "y"):
-            add_city(city, users_city)
+            add_city(weather_for_city, users_city)
 
             new_city = input("Would you like to enter another city? (Y/N)")
 
