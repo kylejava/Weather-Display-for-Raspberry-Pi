d9a798ed24f7bd66f131936dc4d0870ae804d0e4
JVAVmusic <kyleedwardgjava@gmail.com>
2019-03-13 20:28:57 -0700

    fixed for loop

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import sys
 from pprint import pprint
 
 def add_city(weather_for_city , users_city):
-    city = raw_input("Enter name of city: ")
+    city = input("Enter name of city: ")
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=3371747f2cc96f96f53f3da617aa3f91&units=metric'.format(city)
     res = requests.get(url)
     data = res.json()
@@ -23,9 +23,9 @@ def add_city(weather_for_city , users_city):
 def main():
     LCD = True
     users_city = []
-    city = raw_input("Enter Name of city: ")
+    city = input("Enter Name of city: ")
     users_city.append(city)
-    new_city = raw_input("Would you like to enter another city? (Y/N)")
+    new_city =input("Would you like to enter another city? (Y/N)")
 
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=3371747f2cc96f96f53f3da617aa3f91&units=metric'.format(city)
     res = requests.get(url)
@@ -45,7 +45,7 @@ def main():
         while(new_city == "y"):
             add_city(weather_for_city, users_city)
 
-            new_city = raw_input("Would you like to enter another city? (Y/N)")
+            new_city = input("Would you like to enter another city? (Y/N)")
 
         while(LCD == True):
             users_city_len = len(users_city)
@@ -53,7 +53,11 @@ def main():
                 while (i != users_city_len):
                     print("Time" + datetime.datetime.utcnow().strftime("%H:%M:%S"))
                     print(("Weather in ") + str(users_city[i]) + (": ")+ (str(weather_for_city[i])))
-                    i += 1
+                    i +=1
+                    if(i == users_city_len):
+                        i = 0
+                    else:
+                        pass
 
 
 
